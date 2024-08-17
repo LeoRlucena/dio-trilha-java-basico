eae680c9e3b5597aafead8cf6130876da88e5055
Leonardo Lucena <leorlucena@gmail.com>
2024-08-17 11:54:19 -0300

    metade do curso alcançado

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Constantes: os valores não vão mudar mais.
+
+        String meuNome = "Leonardo Lucena";
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(meuNome);
+        System.out.println(VALOR_DE_PI);
     }
 }
